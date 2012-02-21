780ac8d9f6abc2c49207df22e8249b0eee59dd36
Mark Gardner <mjgardner@cpan.org>
2012-02-21 13:48:05 -0500

    initial commit

---
+++ lib/SVN/Tree.pm
@@ -0,0 +1,20 @@
+use utf8;
+use Modern::Perl;
+
+package SVN::Tree;
+use strict;
+
+# VERSION
+use English '-no_match_vars';
+
+1;
+
+# ABSTRACT: Main module for SVN-Tree
+
+=head1 SYNOPSIS
+
+    use SVN::Tree;
+
+=head1 DESCRIPTION
+
+Describe your module here.
